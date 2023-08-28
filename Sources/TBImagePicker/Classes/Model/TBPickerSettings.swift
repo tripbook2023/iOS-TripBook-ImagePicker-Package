faf27fadac6a41911b23a72d3d3771fd7df09285
Safari <111dltldnjs@gmail.com>
2023-08-28 20:58:37 +0900

    feat: TBPickerSettings 구현

---
+++ Sources/TBImagePicker/Classes/Model/TBPickerSettings.swift
@@ -0,0 +1,50 @@
+//
+//  TBPickerSettings.swift
+//  
+//
+//  Created by 이시원 on 2023/08/28.
+//
+
+import Photos
+
+enum TBPickerSelection {
+    case single
+    case multiple(max: Int?, min: Int?)
+}
+
+public final class TBPickerFetchOptions {
+  let options = PHImageRequestOptions()
+  
+  public var isSynchronous: Bool {
+    get {
+      return options.isSynchronous
+    }
+    set {
+      options.isSynchronous = newValue
+    }
+  }
+  
+  public var deliveryMode: PHImageRequestOptionsDeliveryMode {
+    get {
+      return options.deliveryMode
+    }
+    set {
+      options.deliveryMode = newValue
+    }
+  }
+}
+
+public final class TBPickerSettings {
+    static let shared = TBPickerSettings()
+    
+    public let fetchOptions: TBPickerFetchOptions
+    var selection: TBPickerSelection
+    
+    init(
+        fetchOptions: TBPickerFetchOptions = .init(),
+        selection: TBPickerSelection = .single
+    ) {
+        self.fetchOptions = fetchOptions
+        self.selection = selection
+    }
+}
