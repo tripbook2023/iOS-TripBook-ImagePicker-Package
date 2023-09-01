af9dea82d879904c075c1abd2fdefd1cc9450c17
Safari <111dltldnjs@gmail.com>
2023-09-01 22:26:47 +0900

    refactor: setting 값에 따라 Indicator content가 보이도록 수정

--- Sources/TBImagePicker/Classes/View/TBSelectionIndicator.swift
+++ Sources/TBImagePicker/Classes/View/TBSelectionIndicator.swift
@@ -15,22 +15,23 @@ private class NotHitView: UIView {
     }
 }
 
-final class TBSelectionIndicator: UIButton {
-    private let circle: NotHitView = {
-        let view = NotHitView()
-        return view
-    }()
-    
-    private let numberLabel: UILabel = {
+fileprivate final class IndicatorContentView: NotHitView {
+    let numberLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
+        label.textColor = .white
         return label
+    }()
+    
+    let checkImageView: UIImageView = {
+        let view = UIImageView(image: .init(named: "check", in: .module, with: nil))
+        view.tintColor = .white
+        return view
     }()
     
     init(size: CGFloat) {
         super.init(frame: .zero)
         configureUI(size: size)
-        setNumber(nil)
     }
     
     required init?(coder: NSCoder) {
@@ -38,8 +39,54 @@ final class TBSelectionIndicator: UIButton {
     }
     
     private func configureUI(size: CGFloat) {
+        backgroundColor = .clear
+        switch TBPickerSettings.shared.selection {
+        case .single:
+            checkImageView.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(checkImageView)
+            NSLayoutConstraint.activate([
+                checkImageView.widthAnchor.constraint(equalToConstant: size * 0.9),
+                checkImageView.heightAnchor.constraint(equalToConstant: size * 0.9),
+                checkImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+                checkImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+            ])
+        case .multiple(_, _):
+            numberLabel.translatesAutoresizingMaskIntoConstraints = false
+            numberLabel.font = .systemFont(ofSize: size * 0.5)
+            addSubview(numberLabel)
+            NSLayoutConstraint.activate([
+                numberLabel.topAnchor.constraint(equalTo: self.topAnchor),
+                numberLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+                numberLabel.leftAnchor.constraint(equalTo: self.leftAnchor),
+                numberLabel.rightAnchor.constraint(equalTo: self.rightAnchor)
+            ])
+        }
+    }
+}
+
+final class TBSelectionIndicator: UIButton {
+    private let size: CGFloat
+    
+    private let circle: NotHitView = {
+        let view = NotHitView()
+        return view
+    }()
+    
+    private lazy var contentView = IndicatorContentView(size: size)
+    
+    init(size: CGFloat) {
+        self.size = size
+        super.init(frame: .zero)
+        configureUI()
+        setNumber(nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
         addSubview(circle)
-        addSubview(numberLabel)
         let circleSize: CGFloat = size
         circle.layer.cornerRadius = circleSize / 2.0
         circle.translatesAutoresizingMaskIntoConstraints = false
@@ -49,26 +96,24 @@ final class TBSelectionIndicator: UIButton {
             circle.widthAnchor.constraint(equalToConstant: circleSize),
             circle.heightAnchor.constraint(equalToConstant: circleSize)
         ])
-        
-        numberLabel.translatesAutoresizingMaskIntoConstraints = false
-        numberLabel.font = .systemFont(ofSize: size * 0.5)
+        addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            numberLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            numberLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+            contentView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            contentView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
         ])
     }
     
     func setNumber(_ number: Int?) {
-        numberLabel.isHidden = (number == nil)
+        contentView.isHidden = (number == nil)
         circle.layer.borderWidth = 1
-        circle.layer.borderColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1).cgColor
+        circle.layer.borderColor = UIColor(r: 255, g: 255, b: 255).cgColor
         if let number = number {
-            circle.backgroundColor = UIColor(red: 255, green: 78, blue: 22, alpha: 1)
-            numberLabel.textColor = .white
-            numberLabel.text = "\(number)"
+            circle.backgroundColor = UIColor(r: 255, g: 78, b: 22)
+            contentView.numberLabel.text = "\(number)"
         } else {
-            circle.backgroundColor = UIColor(red: 225, green: 220, blue: 219, alpha: 1)
-            numberLabel.text = ""
+            circle.backgroundColor = UIColor(r: 255, g: 220, b: 219)
+            contentView.numberLabel.text = ""
         }
     }
 }
